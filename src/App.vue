920e7571d96c690e8b8bbddad019d4d8ae5043c6
unknown <jorge.zanela19@gmail.com>
2021-09-06 14:12:08 -0300

    Tarefas

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <h2>{{ titulo }}</h2>
+    <div class="input-group">
+      <input @keyup.enter="adtarefa" v-model="novatarefa" type="text" />
+      <span class="input-group-btn">
+        <button @click="adtarefa" class="btn btn-primary">Adicionar</button>
+      </span>
+    </div>
+    <ul>
+      <li
+        v-for="(tarefa, index) in tarefas"
+        :key="index"
+        :class="{ removed: tarefa.checked }"
+      >
+        <input type="checkbox" v-model="tarefa.checked" />
+        <label>{{ tarefa.titulo }}</label>
+      </li>
+    </ul>
+    <footer>
+      <em>Altere aqui o título da sua lista</em>
+      <input v-model="titulo" type="text" />
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      titulo: "Minha lista de tarefas KEKW",
+      novatarefa: "",
+      tarefas: [
+        { titulo: "estudar", checked: false },
+        { titulo: "passar o nofap", checked: true },
+      ],
+    };
+  },
+  methods: {
+    adtarefa() {
+      this.tarefas.push({
+        titulo: this.novatarefa,
+        checked: false,
+      });
+      this.novatarefa='';
+    }
+  }
+};
+</script>
+
+<style>
+.container {
+  width: 40%;
+  margin: 40px auto 20px auto;
+}
+ul li {
+  list-style: none;
+}
+.removed {
+  color: gray; 
+}
+.removed label {
+  text-decoration: line-through;
+}
+</style>
